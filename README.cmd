3950e0d301f2f328843a650aad15c8bea20ebb29
arcalderon <augustin.r.calderon@gmail.com>
2019-11-25 21:10:14 -0500

    Adding Readme

---
+++ README.cmd
@@ -0,0 +1,33 @@
+# directorymerge
+
+
+### Installation
+
+
+
+```
+git clone git@github.com:arcalderon/coding-challenge.git
+
+```
+
+
+###### Directory Merge - Using IDE (Passing No Arguments: Program Will Prompt for User Input)
+```
+Step 1: Open MergeDirectoryFiles located in /directorymerge/src/main/scala/example/MergeDirectoryFiles.scala
+Step 2: Right Click + 'Run MergeDirectoryFiles'
+Step 3: Please Enter input data location:
+i.e  Please Enter input data location: input
+Step 3: Please Enter output data location: output
+i.e  Please Enter output data location: input
+```
+
+###### Directory Merge - Using IDE (Passing Arguments)
+```
+Step 1: Enter Program Arguments as such: merge /path/directorymerge/input /path/directorymerge/output
+Step 1: Open MergeDirectoryFiles located in /directorymerge/src/main/scala/example/MergeDirectoryFiles.scala
+Step 2: Right Click + 'Run MergeDirectoryFiles'
+
+```
+
+
+
